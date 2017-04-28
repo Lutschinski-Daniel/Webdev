35ea8b59ad98b02dc0f5851c2be9857664be3fee
Daniel Lutschinski <d.lutschinski@web.de>
2017-04-28 09:52:07 +0200

    Updated composer. Added second way to get reques-variables.

--- src/AppBundle/Controller/VocabController.php
+++ src/AppBundle/Controller/VocabController.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Vocab;
 
@@ -23,11 +23,14 @@ class VocabController extends Controller {
     /**
      * @Route ("/projects/vocabs/create", name="create")
      */
-    public function createVocabAction() {
-        $em = $this->getDoctrine()->getManager();
- 
-        $ger = filter_input(INPUT_GET, 'german');
-        $for = filter_input(INPUT_GET, 'foreign');
+    public function createVocabAction(Request $request) {
+        //one possibility to do it
+        //$ger = filter_input(INPUT_GET, 'german');
+        //$for = filter_input(INPUT_GET, 'foreign');
+        
+        // better possibility to do it
+        $ger = $request->query->get('german');
+        $for = $request->query->get('foreign');
 
         if(empty($ger) || empty($for) ){
             return $this->redirectToRoute('vocabs');
@@ -36,6 +39,7 @@ class VocabController extends Controller {
          /** @var $vocab Vocab */
         $vocab = new Vocab($ger, $for);
 
+        $em = $this->getDoctrine()->getManager();
         $em->persist($vocab);
         $em->flush();
 
